1eb9d97870bb3c603ea3e00e0951dfdcd5f1a125
nchao <nguyenchuong.hao@mobifos.co>
2026-03-07 13:46:33 +0700

    chore(init): setup React TypeScript Vite project with Tailwind CSS v4

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-blue-50">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Smart MES Frontend Ready! 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
